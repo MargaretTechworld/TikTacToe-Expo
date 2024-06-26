dd990811f51a94bcd84fa5e1d2a4560e245eb37f
MargaretTechworld <margarettechworld@gmail.com>
2024-06-26 14:34:48 +0000

    Style and Update the Rules screen

--- src/screens/Rules.js
+++ src/screens/Rules.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
+import Title from '../component/Title'
+import Message from '../component/Message'
+import GameRules from '../../data/gameRules'
+import TButton from '../component/TButton'
 
-const Rules = () => {
+
+const Rules = ({navigation}) => {
+  const NavigateToHome=() => navigation.navigate("Home")
   return (
-    <View>
-      <Text>Rules</Text>
+    <View style={styles.rulesContainer}>
+      <Title label={"Rules"} />
+      <View>
+        <Message  message={GameRules}/>
+      </View>
+      <View style={styles.buttonSection}>
+        <TButton label={"Back"} onPress={NavigateToHome}/>
+      </View>
     </View>
   )
 }
 
 export default Rules
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rulesContainer: {
+    padding: 25,
+  },
+  buttonSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+})
